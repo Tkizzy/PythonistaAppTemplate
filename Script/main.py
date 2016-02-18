880d6f1d6f0ef7588bfbdcd92c5f2481edfbc0bd
element94 <altkinx@gmail.com>
2016-02-17 20:38:01 -0800

    init commit
    changed organization, deployment target to 8.1, and name of Target. Compiles fine on 64 bit devices.
    
    Also changed bundle identifier to existing one from previous project.
    
    Also added to .gitignore including user interface state

--- Script/main.py
+++ Script/main.py
@@ -2,5 +2,11 @@
 
 # Put your script (and all resources that it needs for running) in this folder.
 # The filename of the main script should be "main.py".
+from objc_util import NSBundle
 
+info = NSBundle.mainBundle().infoDictionary()
+version = str(info['CFBundleVersion'])
+short_version = str(info['CFBundleShortVersionString'])
+print(version)
+print(short_version)
 print('Hello World')
